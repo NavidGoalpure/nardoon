6d3521dbf0474a94b5c5e324aafd1dd3e8a64fa1
Navid Goalpure <Navid.goalpure@gmail.com>
2019-01-19 13:20:21 +0330

    first commit

---
+++ config/site.js
@@ -0,0 +1,43 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+
+  siteTitle: 'تقدیم به امیلیا', // Navigation and Site Title
+  siteTitleAlt: 'سوالاتی برای دورهمی یا قرارهای عاشقانه ', // Alternative Site title for SEO
+  siteTitleShort: 'امیلیا', // short_name for manifest
+  siteHeadline: 'سوالاتی برای دورهمی یا قرارهای عاشقانه ', // Headline for schema.org JSONLD
+  siteUrl: 'https://emilia.lekoarts.de', // Domain of your site. No trailing slash!
+  siteLanguage: 'fa', // Language Tag on <html> element
+  siteLogo: '/logos/logo.png', // Used for SEO and manifest
+  siteDescription:
+    'سوالاتی که می‌تونین در قرارهای عاشقانه یا دورهمی‌ها از همدیگه‌بپرسین تا لحظات جذاب‌تری رو داشته باشین',
+  author: 'Navid', // Author for schema.org JSONLD
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: '@emilia', // Twitter Username
+  ogSiteName: 'emilia', // Facebook Site Name
+  ogLanguage: 'en_US', // og:language
+  googleAnalyticsID: 'UA-12345689-1',
+
+  // Manifest and Progress color
+  themeColor: '#3498DB',
+  backgroundColor: '#2b2e3c',
+
+  // Your information
+  avatar: '/logos/lekoarts.png',
+  name: 'LekoArts',
+  location: 'Iran',
+  socialMedia: [
+    {
+      url: 'https://twitter.com/lekoarts_de',
+      name: 'Twitter',
+    },
+    {
+      url: 'https://www.instagram.com/lekoarts.de',
+      name: 'Instagram',
+    },
+    {
+      url: 'https://www.lekoarts.de/en',
+      name: 'Homepage',
+    },
+  ],
+}
---
+++ src/components/ProjectPagination.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import { Link } from 'gatsby'
+
+import arrowLeft from '../images/left-chevron.svg'
+import arrowRight from '../images/right-chevron.svg'
+
+const Wrapper = styled.div`
+  display: flex;
+  max-width: ${props => props.theme.maxWidths.general};
+  margin: 0 auto;
+  padding: 3rem 0;
+  a {
+    color: ${props => props.theme.colors.color};
+    display: flex;
+    align-items: center;
+    font-size: 1.25rem;
+    &:hover {
+      color: ${props => props.theme.colors.linkHover};
+    }
+  }
+  justify-items: center;
+`
+
+const Prev = styled.div`
+  img {
+    width: 40px;
+    height: 40px;
+    margin: 0 1rem 0 0;
+  }
+`
+
+const Next = styled.div`
+  img {
+    width: 40px;
+    height: 40px;
+    margin: 0 0 0 1rem;
+  }
+  margin-left: auto;
+`
+
+const ProjectPagination = ({ next, prev }) => (
+  <Wrapper>
+    {next && (
+      <Next>
+        <Link to={next.fields.slug}>
+          <img src={arrowRight} alt="Arrow Right" />
+          <h3>{next.frontmatter.title}</h3>
+        </Link>
+      </Next>
+    )}
+    {prev && (
+      <Prev>
+        <Link to={prev.fields.slug}>
+          <h3> {prev.frontmatter.title}</h3>
+          <img src={arrowLeft} alt="Arrow Left" />
+        </Link>
+      </Prev>
+    )}
+  </Wrapper>
+)
+
+export default ProjectPagination
+
+ProjectPagination.propTypes = {
+  next: PropTypes.object,
+  prev: PropTypes.object,
+}
+
+ProjectPagination.defaultProps = {
+  next: null,
+  prev: null,
+}
