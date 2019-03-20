edd7a76604efcc183df1e3b740fab7949c9344fb
Navid Goalpure <Navid.goalpure@gmail.com>
2019-03-20 16:19:39 +0330

    improve

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,9 @@ import Footer from './Footer'
 import Header from './Header'
 import Layout from './Layout'
 import ProjectHeader from './ProjectHeader'
+import RandomBtn from './RandomBtn'
+
 import ProjectPagination from './ProjectPagination'
 import SEO from './SEO'
 
-export { Card, Footer, Header, Layout, ProjectHeader, ProjectPagination, SEO }
+export { Card, Footer, Header, Layout, ProjectHeader, ProjectPagination, SEO, RandomBtn }
