da5b8b6ce468ec37e7b8ef903e3636e924c8f5a9
Navid Goalpure <Navid.goalpure@gmail.com>
2019-03-31 22:30:50 +0430

    temporary save

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,34 +32,40 @@ const BG = styled.div`
   background-color: ${props => props.theme.colors.bg};
 `
 
-const Index = ({ data: { allMdx, multiChance } }) => (
-  <Layout>
-    <Header
-      avatar={config.avatar}
-      name={config.name}
-      siteDescription={config.siteDescription}
-      socialMedia={config.socialMedia}
-    />
+const Index = ({ data: { filterOnYearkDay, filterOnWeekDay, multiChance } }) => {
+  const filterdCatagories = [...filterOnWeekDay.edges, ...filterOnYearkDay.edges]
+  console.log('filterOnYearkDay=', filterOnYearkDay)
+  console.log('filterOnWeekDay=', filterOnWeekDay)
+  console.log('filterdCatagories=', filterdCatagories)
+  return (
+    <Layout>
+      <Header
+        avatar={config.avatar}
+        name={config.name}
+        siteDescription={config.siteDescription}
+        socialMedia={config.socialMedia}
+      />
 
-    <BG>
-      <Content>
-        <Grid>
-          <ChanceCard delay={0} cover={multiChance.edges[0].node.fluid} path="/RandomPage/" />
-          {allMdx.edges.map((project, index) => (
-            <Card
-              delay={index}
-              title={project.node.frontmatter.title}
-              cover={project.node.frontmatter.cover.childImageSharp.fluid}
-              path={project.node.fields.slug}
-              areas={project.node.frontmatter.areas}
-              key={project.node.fields.slug}
-            />
-          ))}
-        </Grid>
-      </Content>
-    </BG>
-  </Layout>
-)
+      <BG>
+        <Content>
+          <Grid>
+            <ChanceCard delay={0} cover={multiChance.edges[0].node.fluid} path="/RandomPage/" />
+            {filterdCatagories.map((project, index) => (
+              <Card
+                delay={index}
+                title={project.node.frontmatter.title}
+                cover={project.node.frontmatter.cover.childImageSharp.fluid}
+                path={project.node.fields.slug}
+                areas={project.node.frontmatter.areas}
+                key={project.node.fields.slug}
+              />
+            ))}
+          </Grid>
+        </Content>
+      </BG>
+    </Layout>
+  )
+}
 
 export default Index
 
@@ -70,10 +76,38 @@ Index.propTypes = {
     }),
   }).isRequired,
 }
-
+// به نظر میاد گتبزبی راهی برای تعریف عملگر «یا» نداره. به همین دلیل مجبور شدم دوبار کوئری بزنم
+// یکبار برای پست هایی که با روزهای هفته فیلتر میشن و یکبار برای پست هایی که با تاریخ سالانه فیلتر می‌شن
+// درنهایت این دو خروجی رو در یک آرایه جمع می‌کنم
 export const pageQuery = graphql`
   query HomeQuery {
-    allMdx(sort: { fields: [frontmatter___title], order: DESC }) {
+    filterOnYearkDay: allMdx(
+      filter: { frontmatter: { limitDate: { eq: "Sunday" } } }
+      sort: { fields: [frontmatter___title], order: DESC }
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            cover {
+              childImageSharp {
+                fluid(maxWidth: 760, quality: 90) {
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+            }
+            title
+            areas
+          }
+        }
+      }
+    }
+    filterOnWeekDay: allMdx(
+      filter: { frontmatter: { limitDate: { eq: "Saturday" } } }
+      sort: { fields: [frontmatter___title], order: DESC }
+    ) {
       edges {
         node {
           fields {
